e4ead5a1844ba030a08465cf1f72de886c666d14
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-10-22 13:12:44 -0700

    add business logic and answer card

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,48 @@ optionA=0;
 optionB=0;
 optionC=0;
 counter=0;
+target=0;
+const questions = ["♫ you like pina coladas ♫", "♫ you enjoy getting caught in the rain ♫","♫ you're not into yoga ♫", "♫ you have half a brain ♫", "♫ You're not into healthfood ♫", "♫ you are into champagne ♫","♫ you prefer not to be stopped when you're having such a good time, or when you're havin' a ball ♫","♫ we are human ♫", "♫ we are dancer ♫","♫ my sign is vital ♫","♫ my hands are cold ♫"]
 
 function updateQuestion() {
-  $("#questionContent").text(questions[counter]);
+  $("#questionContent").text(questions[counter-1]);
+  counter += 1;
+}
+function logAnswer() {
+  let target = ((counter-1)%3)+1;
+  if (target===1){
+    optionA+=$("input:radio[name=userInput]:checked").val();
+  }else if (target===2){
+    optionB+=$("input:radio[name=userInput]:checked").val();
+  }else{
+    optionC+=$("input:radio[name=userInput]:checked").val();
+  }
+}
+function results(){
+  if (optionA>optionB&&optionB>=optionC){
+    return "Result A";
+  }else if (optionB>optionC&&optionC>=optionA){
+    return "Result B"
+  }else{
+    return "Result C"
+  }
 }
 
-
-const questions = ["♫ you like pina coladas ♫", "♫ you enjoy getting caught in the rain ♫","♫ you're not into yoga ♫", "♫ you have half a brain ♫", "♫ You're not into healthfood ♫", "♫ you are into champagne ♫","♫ you prefer not to be stopped when you're having such a good time, or when you're havin' a ball ♫","♫ we are human ♫", "♫ we are dancer ♫","♫ my sign is vital ♫","♫ my hands are cold ♫"]
 $(document).ready(function(){
   $("#startButton").click(function(){
     $(".titleCard").addClass("hidden");
     $(".questionCard").removeClass("hidden")
-    counter = 0;
     updateQuestion();
   });
   $("#nextButton").click(function(){
-
-    // This is where the call to the function will be
-    counter+=1;
     $("#default").prop("checked",true);
     //verified this counter works
+    if (counter > questions.length()){
+      $(".questionCard").addClass("hidden");
+      $(".answerCard").removeClass("hidden")
+      $("#answer").text(results());
+    }else{
     updateQuestion()
-    console.log(((counter-1)%3)+1);
+    }
   });
 });
