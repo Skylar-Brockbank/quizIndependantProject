cacce7c14b985ed22f8d4ff9f6869f96e836e62f
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-10-22 15:30:52 -0700

    save the first fully functional prototype

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@ optionB=0;
 optionC=0;
 counter=0;
 target=0;
+const outcome = ["Result A","Result B","Result C"];
+const outcomeDescription = ["description A Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, alias illo dolorem suscipit excepturi a velit vel blanditiis dicta voluptas!","description B Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, alias illo dolorem suscipit excepturi a velit vel blanditiis dicta voluptas!","description C Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, alias illo dolorem suscipit excepturi a velit vel blanditiis dicta voluptas!"];
 const questions = ["♫ you like pina coladas ♫", "♫ you enjoy getting caught in the rain ♫","♫ you're not into yoga ♫", "♫ you have half a brain ♫", "♫ You're not into healthfood ♫", "♫ you are into champagne ♫","♫ you prefer not to be stopped when you're having such a good time, or when you're havin' a ball ♫","♫ we are human ♫", "♫ we are dancer ♫","♫ my sign is vital ♫","♫ my hands are cold ♫"]
+
 
 function updateQuestion() {
   counter += 1;
@@ -14,26 +17,19 @@ function logAnswer() {
   test = parseInt(((counter-1)%3)+1);
   if (test===1){
     optionA += parseInt($("input:radio[name=userInput]:checked").val());
-    console.log("I ran")
   }else if (test===2){
     optionB += parseInt($("input:radio[name=userInput]:checked").val());
   }else{
     optionC += parseInt($("input:radio[name=userInput]:checked").val());
   }
-  console.log(test===1);
-  console.log(test===2);
-  console.log(test===3);
-  console.log(optionA);
-  console.log(optionB);
-  console.log(optionC);
 }
 function results(){
   if (optionA>optionB&&optionB>=optionC){
-    return "Result A";
+    return 0;
   }else if (optionB>optionC&&optionC>=optionA){
-    return "Result B";
+    return 1;
   }else{
-    return "Result C";
+    return 2;
   }
 }
 
@@ -46,7 +42,8 @@ $(document).ready(function(){
   $("#nextButton").click(function(){
     if (counter > (questions.length-1)){
       $(".questionCard").addClass("hidden");
-      $("#answer").text(results());
+      $("#answer").text(outcome[results()]);
+      $("#description").text(outcomeDescription[results()]);
       $(".answerCard").removeClass("hidden");
     }else{
     logAnswer();
