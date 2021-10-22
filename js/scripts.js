f42aa8bff9b1ba44ac7cbc3d369410f3c70d84ca
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-10-22 12:30:19 -0700

    add basic page turning logic, and set ground for business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
+optionA=0;
+optionB=0;
+optionC=0;
+counter=0;
+
+function updateQuestion() {
+  $("#questionContent").text(questions[counter]);
+}
+
+
+const questions = ["♫ you like pina coladas ♫", "♫ you enjoy getting caught in the rain ♫","♫ you're not into yoga ♫", "♫ you have half a brain ♫", "♫ You're not into healthfood ♫", "♫ you are into champagne ♫","♫ you prefer not to be stopped when you're having such a good time, or when you're havin' a ball ♫","♫ we are human ♫", "♫ we are dancer ♫","♫ my sign is vital ♫","♫ my hands are cold ♫"]
 $(document).ready(function(){
   $("#startButton").click(function(){
     $(".titleCard").addClass("hidden");
     $(".questionCard").removeClass("hidden")
+    counter = 0;
+    updateQuestion();
+  });
+  $("#nextButton").click(function(){
+
+    // This is where the call to the function will be
+    counter+=1;
+    $("#default").prop("checked",true);
+    //verified this counter works
+    updateQuestion()
+    console.log(((counter-1)%3)+1);
   });
 });
